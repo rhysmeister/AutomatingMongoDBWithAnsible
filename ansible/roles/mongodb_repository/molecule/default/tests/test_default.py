6685b41b92843db4c3fd9574350c2b0055a4018c
Rhys Campbell <rhys.james.campbell@googlemail.com>
2020-01-29 13:36:04 +0100

    Changes for PEP8

--- ansible/roles/mongodb_repository/molecule/default/tests/test_default.py
+++ ansible/roles/mongodb_repository/molecule/default/tests/test_default.py
@@ -6,11 +6,13 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']
 ).get_hosts('all')
 
-# Note: The MongoDB Version is hard codes here. TODO Can we access the role variables in TestInfra?
+# Note: The MongoDB Version is hard codes here.
+# TODO Can we access the role variables in TestInfra?
+
 
 def test_redhat_mongodb_repository_file(host):
     if host.system_info.distribution == "redhat" \
-        or host.system_info.distribution == "centos":
+            or host.system_info.distribution == "centos":
         f = host.file("/etc/yum.repos.d/mongodb-org-4.2.repo")
         assert f.exists
         assert f.user == 'root'
@@ -18,10 +20,12 @@ def test_redhat_mongodb_repository_file(host):
         assert f.mode == 0o644
         assert f.md5sum == "8e09a9eaf2bebcb24f417ab86235bf70"
 
+
 def test_redhat_yum_search(host):
     if host.system_info.distribution == "redhat" \
-        or host.system_info.distribution == "centos":
-        cmd = host.run("yum search mongodb-org --disablerepo='*' --enablerepo='mongodb-org-4.2'")
+            or host.system_info.distribution == "centos":
+        cmd = host.run("yum search mongodb-org --disablerepo='*' \
+                            --enablerepo='mongodb-org-4.2'")
 
         assert cmd.rc == 0
         assert "mongodb-org.x86_64" in cmd.stdout
@@ -33,7 +37,7 @@ def test_redhat_yum_search(host):
 
 def test_debian_mongodb_repository_file(host):
     if host.system_info.distribution == "debian" \
-        or host.system_info.distribution == "ubuntu":
+            or host.system_info.distribution == "ubuntu":
         f = host.file("/etc/apt/sources.list.d/mongodb-org-4.2.list")
 
         assert f.exists
@@ -48,7 +52,7 @@ def test_debian_mongodb_repository_file(host):
 
 def test_debian_apt_search(host):
     if host.system_info.distribution == "debian" \
-        or host.system_info.distribution == "ubuntu":
+            or host.system_info.distribution == "ubuntu":
         cmd = host.run("apt search mongodb-org")
 
         assert cmd.rc == 0
