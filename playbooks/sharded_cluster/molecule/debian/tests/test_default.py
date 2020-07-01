7cb8968ff39fa7e7d792b1f1b4c779b64c8171c5
Rhys Campbell <rhys.james.campbell@googlemail.com>
2020-07-01 18:07:07 +0200

    Additional sharded cluster tests

--- playbooks/sharded_cluster/molecule/debian/tests/test_default.py
+++ playbooks/sharded_cluster/molecule/debian/tests/test_default.py
@@ -32,4 +32,26 @@ def test_mongod_replicaset(host):
         assert "mongodb5.local:{0}".format(port) in r.stdout
         assert "mongodb6.local:{0}".format(port) in r.stdout
 
-# TODO Verification for mongos
+
+def test_mongo_services(host):
+    if host.ansible.get_variables()['inventory_hostname'].startswith("mongodb"):
+        service = host.service("mongod")
+        assert service.is_running
+        assert service.is_enabled
+    elif host.ansible.get_variables()['inventory_hostname'].startswith("mongos"):
+        # mongos servers also have a config server mongod
+        service = host.service("mongos")
+        assert service.is_running
+        assert service.is_enabled
+        service = host.service("mongod")
+        assert service.is_running
+        assert service.is_enabled
+
+
+def test_mongos_shards(host):
+    if host.ansible.get_variables()['inventory_hostname'].startswith("mongos"):
+        port = 27017
+        cmd = "mongo --port {0} --username admin --password secret --eval 'sh.status()'".format(port)
+        r = host.run(cmd)
+        assert 'rs0' in r.stdout
+        assert 'rs1' in r.stdout
