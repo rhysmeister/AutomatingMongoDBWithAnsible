18520257aac588a816f7b7ceb2c7553ff440a112
Rhys Campbell <rhys.james.campbell@googlemail.com>
2020-09-02 12:30:49 +0200

    Add reboot test

--- playbooks/replicaset/tests_mongodbreplicaset.py
+++ playbooks/replicaset/tests_mongodbreplicaset.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import testinfra.utils.ansible_runner
 
@@ -22,3 +23,22 @@ def test_mongodb_replicaset(host):
     assert "mongodb1.local:27017" in cmd.stdout
     assert "mongodb2.local:27017" in cmd.stdout
     assert "mongodb3.local:27017" in cmd.stdout
+
+
+def test_mongodb_reboot(host):
+    '''
+    Reboot the host and check the mongod service comes back up
+    '''
+    #rebooted = host.ansible("reboot", "msg='Reboot initiated from Testinfra.'", become=True, become_user="root")
+    #assert rebooted["rebooted"]
+    # This version works
+    #host.ansible("command", "reboot", check=False, become=True)
+    host.run("sudo reboot")
+    time.sleep(60)
+
+    service = host.service("mongod")
+    assert service.is_running
+    assert service.is_enabled
+
+    socket = host.socket("tcp://0.0.0.0:27017")
+    assert socket.is_listening
