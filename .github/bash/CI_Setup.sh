ab02e2d9036aad7fd4326944a5261834c3aba72d
Rhys Campbell <rhys.james.campbell@googlemail.com>
2021-03-04 17:29:04 +0100

    Update script logic

--- .github/bash/CI_Setup.sh
+++ .github/bash/CI_Setup.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 set -e;
+set -u;
 
 sudo apt-get update;
 python --version;
 pip install --upgrade pip;
 pip install --requirement requirements.txt;
-if [ -n "$LATEST" ]; then
-  ansible-galaxy collection install community.general;
+if [ "$COLLECTION" == "latest" ]; then
   wget https://github.com/ansible-collections/community.mongodb/releases/download/latest/community-mongodb-latest.tar.gz;
   ansible-galaxy collection install community-mongodb-latest.tar.gz;
+elif [ "$COLLECTION" == "requirements.yml" ]; then
+  ansible-galaxy collection install -r requirements.yml;
 else
-  ansible-galaxy collection install -r requirements.yml
+  echo "Invalid value for COLLECTION given";
+  exit 1;
 fi;
 pip --version;
 ansible --version;
