ea24f3f4ab1c093c4a30f52f14c329c572430852
Rhys Campbell <rhys.james.campbell@googlemail.com>
2021-03-04 17:12:32 +0100

    Run CI with Galaxy released version of collection

--- .github/bash/CI_Setup.sh
+++ .github/bash/CI_Setup.sh
@@ -5,9 +5,13 @@ sudo apt-get update;
 python --version;
 pip install --upgrade pip;
 pip install --requirement requirements.txt;
-ansible-galaxy collection install community.general;
-wget https://github.com/ansible-collections/community.mongodb/releases/download/latest/community-mongodb-latest.tar.gz;
-ansible-galaxy collection install community-mongodb-latest.tar.gz;
+if [ -n "$LATEST" ]; then
+  ansible-galaxy collection install community.general;
+  wget https://github.com/ansible-collections/community.mongodb/releases/download/latest/community-mongodb-latest.tar.gz;
+  ansible-galaxy collection install community-mongodb-latest.tar.gz;
+else
+  ansible-galaxy collection install -r requirements.yml
+fi;
 pip --version;
 ansible --version;
 molecule --version;
