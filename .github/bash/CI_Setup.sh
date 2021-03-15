5ff48e5a8ec9e19ad7cc3cc58d975fa3113f8665
Rhys Campbell <rhys.james.campbell@googlemail.com>
2021-03-15 20:23:15 +0100

    Update CI script for Python 2.7

--- .github/bash/CI_Setup.sh
+++ .github/bash/CI_Setup.sh
@@ -5,7 +5,12 @@ set -u;
 sudo apt-get update;
 python --version;
 pip install --upgrade pip;
-pip install --requirement requirements.txt;
+export pyv=$(python -c 'from platform import python_version; print(python_version()[:3])');
+if [ "$pyv" == "2.7" ]; then
+  pip install --requirement requirements-2.7.txt;
+else
+  pip install --requirement requirements.txt;
+fi;
 if [ "$COLLECTION" == "latest" ]; then
   wget https://github.com/ansible-collections/community.mongodb/releases/download/latest/community-mongodb-latest.tar.gz;
   ansible-galaxy collection install community-mongodb-latest.tar.gz;
