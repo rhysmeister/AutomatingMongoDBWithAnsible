3c3adcca7091c07c95468953719204bc830d4602
Rhys Campbell <rhys.james.campbell@googlemail.com>
2021-03-21 13:57:17 +0100

    Tidy up file

--- playbooks/standalone/tests_mongodbstandalone.py
+++ playbooks/standalone/tests_mongodbstandalone.py
@@ -22,17 +22,13 @@ def test_mongodb_service(host):
 def test_mongodb_port(host):
     socket = host.socket("tcp://0.0.0.0:27017")
     assert socket.is_listening
-    
+
 
 @pytest.mark.skipif(os.environ.get('MONGO_REBOOT_TEST', '') != 'TRUE', reason="MONGO_VERSION environment variable is not set")
 def test_mongodb_reboot(host):
     '''
     Reboot the host and check the mongod service comes back up
     '''
-    #rebooted = host.ansible("reboot", "msg='Reboot initiated from Testinfra.'", become=True, become_user="root")
-    #assert rebooted["rebooted"]
-    # This version works
-    #host.ansible("command", "reboot", check=False, become=True)
     host.run("sudo reboot")
     time.sleep(60)
 
